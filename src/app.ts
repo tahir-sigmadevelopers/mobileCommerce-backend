e7b3470727394b04c58f964c5fdfc681aea6ad8c
Mohammad Tahir <starghareeb@gmail.com>
2024-10-20 00:48:48 +0500

    fixing deployment issues.......

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@
 import userRoute from './routes/user.js';
 import productRoute from './routes/product.js';
 import orderRoute from './routes/order.js';
-import paymentRoute from './routes/payments.js';
+// import paymentRoute from './routes/payments.js';
 import dashboardRoute from './routes/stats.js';
 
 import express from 'express';
@@ -15,7 +15,7 @@ import { errorMiddleware } from "./middlewares/error.js";
 import NodeCache from 'node-cache';
 import { config } from 'dotenv';
 import morgan from 'morgan';
-import Stripe from 'stripe';
+// import Stripe from 'stripe';
 import cors from 'cors';
 
 config({
@@ -25,9 +25,9 @@ config({
 connectDB();
 
 const port = process.env.PORT || 5000;
-const stripeSecret = process.env.STRIPE_SECRET || "";
+// const stripeSecret = process.env.STRIPE_SECRET || "";
 
-export const stripe = new Stripe(stripeSecret);
+// export const stripe = new Stripe(stripeSecret);
 export const myCache = new NodeCache();
 
 
@@ -48,7 +48,7 @@ app.get("/", (req, res) => {
 app.use("/user", userRoute);
 app.use("/product", productRoute);
 app.use("/order", orderRoute);
-app.use("/payment", paymentRoute);
+// app.use("/payment", paymentRoute);
 app.use("/dashboard", dashboardRoute);
 
 
